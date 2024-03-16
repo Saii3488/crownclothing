9a8ec02fe89c9c4676cc907b272e5de7f332be54
Saii3488 <saiipranav197@gmail.com>
2024-03-16 21:26:39 +0530

    code splitting by dynamic import using lazy and suspense

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import { useEffect } from "react";
+import { useEffect,lazy,Suspense } from "react";
 import { useDispatch } from "react-redux";
 import { Routes, Route, Outlet } from "react-router-dom";
-
-
-import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/Authentication/Authentication.components";
-import Shop from "./routes/shop/shop.components";
-import Checkout from "./routes/checkout/checkout.components";
+import Spinner from "./components/spinner/spinner.components";
 import { checkUserSession } from "./store/user/user.action";
+const Shop=lazy(()=>import("./routes/shop/shop.components"));
+const Checkout=lazy(()=>import("./routes/checkout/checkout.components"));
+const Navigation=lazy(()=>import("./routes/navigation/navigation.component"));
+const Home=lazy(()=>import('./routes/home/home.component'))
+const Authentication=lazy(()=>import('./routes/Authentication/Authentication.components'))
 const App=()=>{
   const dispatch=useDispatch()
   useEffect(()=>{
     dispatch(checkUserSession())
   },[dispatch]);
   return(
-    <Routes>
+    <Suspense fallback={<Spinner />}>
+     <Routes>
       <Route path='/' element={<Navigation />}>
        <Route index element={<Home />} />
        <Route path='shop/*' element={<Shop />} />
@@ -23,10 +23,9 @@ const App=()=>{
        <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
+    </Suspense>
   );
       
 };
-
-
 export default App;
 
